782788df365db3cc9c78ea24c389f73e65c9ae12
Phoenix Bogati <phoenix.bogati@gmail.com>
2026-06-07 14:31:35 -0700

    feat: created a baseline (config.py, main.py,camera.py)

---
+++ src/config.py
@@ -0,0 +1,7 @@
+CAMERA_INDEX = 0
+FRAME_WIDTH = 640
+FRAME_HEIGHT = 480
+FACE_SIZE = (200, 200)
+KNOWN_FACE_DIR = "data/known_faces"
+MODEL_PATH = "models/face_model.yml"
+LABELS_PATH = "models/labels.json"
